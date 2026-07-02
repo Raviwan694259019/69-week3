a5362276a2d51e88ba6315d9b7c37cb0093dca0f
Raviwan694259019 <694259019@webmail.npru.ac.th>
2026-07-02 15:43:23 +0700

    add sum.java

---
+++ week3/Sum.java
@@ -0,0 +1,8 @@
+void main() {
+    IO.print("Enter the number : ");
+    int num1 = Integer.parseInt(IO.readln());
+    IO.print("Enter the number : ");
+    int num2 = Integer.parseInt(IO.readln());
+    int sum = num1 + num2;
+    IO.println("Grand total : " + sum);
+}
